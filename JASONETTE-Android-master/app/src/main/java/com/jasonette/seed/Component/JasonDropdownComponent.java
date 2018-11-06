8bdcfac4a4911365ae76309855b9e00cb916605a
sandhyasrini <sandy150225@gmail.com>
2018-11-07 04:51:38 +0530

    wgitwatch autoCommit 20181107. 45136

--- JASONETTE-Android-master/app/src/main/java/com/jasonette/seed/Component/JasonDropdownComponent.java
+++ JASONETTE-Android-master/app/src/main/java/com/jasonette/seed/Component/JasonDropdownComponent.java
@@ -3,45 +3,105 @@ package com.jasonette.seed.Component;
 import android.content.Context;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
-import android.widget.Switch;
+import android.widget.TextView;
 
-import com.jasonette.seed.Core.JasonViewActivity;
 import com.jasonette.seed.Helper.JasonHelper;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class JasonDropdownComponent {
     public static View build(View view, final JSONObject component, final JSONObject parent, final Context context) {
 
-        if(view == null)
-        {
-         return new Spinner(context);
-        }
-        else {
+        if (view == null) {
+            return new Spinner(context);
+        } else {
             try {
                 view = JasonComponent.build(view, component, parent, context);
                 final Spinner sSpinner = ((Spinner) view);
-                String[] items = new String[]{"1", "2", "three"};
-                if(component.has("name")){
+
+                if (component.has("name")) {
                     Log.i("Name--->", component.getString("name"));
+                    String[] items;
+                    //Log.i("Content Description--->", component.getString("contentDescription"));
 
-                    ArrayAdapter<String> adapter = new ArrayAdapter<>(context, android.R.layout.simple_spinner_dropdown_item, items);
-                    sSpinner.setAdapter(adapter);
+                    int getSize;
 
-                    if(((JasonViewActivity) context).model.var.has(component.getString("name"))){
-                        Log.i("Dropdown", "Inside dropdown");
+                    //Setting Style
+                    final int color;
+                    final float size;
+                    final int padding;
+                    JSONObject style = JasonHelper.style(component, context);
+                    if (style.has("color"))
+                        color = JasonHelper.parse_color(style.getString("color"));
+                    else
+                        color = 0;
 
+                    if (style.has("size"))
+                        size = Float.parseFloat(style.getString("size"));
+                    else
+                        size = 5;
+
+                    if (style.has("padding"))
+                        padding = Integer.parseInt(style.getString("padding"));
+                    else
+                        padding = 0;
+
+
+                    //Setting content description. realwear uses this as voice tag
+                    if (component.has("contentDescription")) {
+                        Log.i("Content Description--->", component.getString("contentDescription"));
+                        ((Spinner) view).setContentDescription(component.getString("contentDescription"));
                     }
-                    else {
 
+
+                    //Create spinner (Dropdown) and map dropdown values.
+                    if (component.has("options")) {
+                        JSONArray arrOfOptions = component.getJSONArray("options");
+                        items = new String[arrOfOptions.length()];
+                        int defaultSelPos = 0;
+                        for (int i = 0; i < arrOfOptions.length(); i++) {
+                            JSONObject optionObj = arrOfOptions.getJSONObject(i);
+                            //Log.i("Dropdown option--->" + i, optionObj.getString("value"));
+                            items[i] = optionObj.getString("value");
+
+                            if (optionObj.has("defaultSelected")) {
+                                //Log.i("defaultSelected -->", optionObj.getString("defaultSelected"));
+                                if (optionObj.getString("defaultSelected").trim().equals("true")) {
+                                    // Log.i("defaultSelected True", optionObj.getString("defaultSelected"));
+                                    defaultSelPos = i;
+                                }
+                            }
+                        }
+
+                        try {
+                            ArrayAdapter<String> adapter = new ArrayAdapter<String>(context, android.R.layout.simple_spinner_dropdown_item, items) {
+                                public View getView(int position, View convertView, ViewGroup parent) {
+                                    View v = super.getView(position, convertView, parent);
+                                    ((TextView) v).setTextSize(size);
+                                    ((TextView) v).setTextColor(color);
+                                    return v;
+                                }
+
+                                public View getDropDownView(int position, View convertView, ViewGroup parent) {
+                                    View v = super.getDropDownView(position, convertView, parent);
+                                    ((TextView) v).setTextSize(size);
+                                    ((TextView) v).setTextColor(color);
+                                    ((TextView) v).setCompoundDrawablePadding(padding);
+                                    return v;
+                                }
+                            };
+
+                            sSpinner.setAdapter(adapter);
+                            sSpinner.setSelection(defaultSelPos);
+                        } catch (Exception e) {
+                            Log.d("Warning", e.getStackTrace()[0].getMethodName() + " : " + e.toString());
+                        }
                     }
                 }
-
-//                JSONObject style = JasonHelper.style(component, context);
-//                String type = component.getString("type");
-
                 return view;
             } catch (Exception e) {
                 Log.d("Warning", e.getStackTrace()[0].getMethodName() + " : " + e.toString());
